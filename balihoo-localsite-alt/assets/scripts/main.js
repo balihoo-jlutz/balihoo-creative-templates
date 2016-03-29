a9106c74b3807e68e7fb12e1759cc585fd618b32
balihoo-dalvarado <dalvarado@balihoo.com>
2016-03-29 00:48:29 -0600

    Update data items
    Updated data items

--- balihoo-localsite-alt/assets/scripts/main.js
+++ balihoo-localsite-alt/assets/scripts/main.js
@@ -1,1 +1,42 @@
-$(function(){$('a[href*="#"]:not([href="#"])').click(function(){if(location.pathname.replace(/^\//,"")===this.pathname.replace(/^\//,"")&&location.hostname===this.hostname){var t=$(this.hash);if(t=t.length?t:$("[name="+this.hash.slice(1)+"]"),t.length)return $("html, body").animate({scrollTop:t.offset().top},1e3),!1}})});
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+  // Format 24-hour BLIP location hours to 12 hour format
+    !function() {
+        var hh, m;
+
+        for (var key in hours) {
+            if (hours.hasOwnProperty(key)) {
+                hh = hours[key].hours;
+                m = hours[key].minutes;
+                if (hh && m) {
+                    var dd = "AM";
+                    var h = hh;
+                    if (h >= 12) {
+                        h = hh-12;
+                        dd = "PM";
+                    }
+                    if (h == 0) {
+                        h = 12;
+                    }
+                    m = m < 10 ? "0" + m:m;
+
+                    document.getElementById(key).innerHTML = " " + h + ":" + m + " " + dd;
+                }
+            }
+
+        }
+    }();
+
+});
